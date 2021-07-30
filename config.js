a9a3844e0160e6bc731f45f96d7132a163bb6784
Prachi Palkar <72156738+PrachiPalkar@users.noreply.github.com>
2021-07-30 13:43:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,7 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 
 let config = {
-    apiKey: "AIzaSyB96LapjUlj3PBf3oDxwSteitDGtYNW5Ro",
-    authDomain: "p-indi.firebaseapp.com",
-    projectId: "p-indi",
-    databaseURL: "https://p-indi-default-rtdb.firebaseio.com",
-    storageBucket: "p-indi.appspot.com",
-    messagingSenderId: "720761999636",
-    appId: "1:720761999636:web:6471e55a8806b51fd0f201"
+    "Enter your firebase credentials"
 };
 
 firebase.initializeApp(config);
